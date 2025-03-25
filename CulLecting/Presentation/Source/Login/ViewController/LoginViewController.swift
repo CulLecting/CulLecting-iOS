31af3c5e8affdd33a2b7d1b0ba10eaae8a6f7875
palrang22 <kseunghui@gmail.com>
2025-03-25 20:05:12 +0900

    [Chore] 파일 초기구조 생성 및 디렉토리 변경

---
+++ CulLecting/Presentation/Source/Login/ViewController/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CulLecting
+//
+//  Created by 김승희 on 3/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
