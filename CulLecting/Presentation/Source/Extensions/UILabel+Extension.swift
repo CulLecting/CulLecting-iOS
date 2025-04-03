00c4ad46c85d9923c39da3ce8c100bc9655724cd
palrang22 <kseunghui@gmail.com>
2025-04-03 14:29:48 +0900

    [Design] ColorSet 추가 / UIFonts, UILabels Extension 추가, 이미지셋 경로 변경

---
+++ CulLecting/Presentation/Source/Extensions/UILabel+Extension.swift
@@ -0,0 +1,34 @@
+//
+//  UILabel+Extension.swift
+//  CulLecting
+//
+//  Created by 김승희 on 4/3/25.
+//
+
+import UIKit
+
+enum LineSpacingStyle {
+    case large
+    case medium
+}
+
+extension UILabel {
+    static func lineSpacingStyle(style: LineSpacingStyle, text: String) -> NSAttributedString
+    {
+        let paragraphStyle = NSMutableParagraphStyle()
+        
+        switch style {
+        case .large:
+            paragraphStyle.minimumLineHeight = 24
+            paragraphStyle.maximumLineHeight = 24
+        case .medium:
+            paragraphStyle.minimumLineHeight = 18
+            paragraphStyle.maximumLineHeight = 18
+            
+        }
+        
+        let attributes: [NSAttributedString.Key: Any] = [.paragraphStyle: paragraphStyle]
+        
+        return NSAttributedString(string: text, attributes: attributes)
+    }
+}
